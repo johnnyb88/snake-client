f625b8b303ce5e8fc2cc2628bfb71dfe0c41006f
johnnyb88 <johnnybarratt@gmail.com>
2019-09-25 22:03:19 +0000

    user input integrated

--- play.js
+++ play.js
@@ -1,25 +1,11 @@
 
 
 const net = require('net');
-const connect = require('./client');
-
-const handleUserInput = function(data) {
-  if (data === '\u0003') {
-    process.exit();
-  }
-};
-
-const setupInput = function() {
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding('utf8');
-  stdin.resume();
-  stdin.on('data',handleUserInput);
-  return stdin;
-};
+const { connect } = require('./client');
+const { setupInput } = require('./input');
 
 
 console.log('Connecting ...');
 connect();
 
-setupInput('data', handleUserInput());
+setupInput();
